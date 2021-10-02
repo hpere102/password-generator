e7bf72b9702677308df9005f033d29d1cbe89fee
danny p <danny@dannys-MacBook-Pro.local>
2021-10-02 18:56:57 -0400

    ordered varibales

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,16 @@ generateBtn.addEventListener("click", writePassword);
  
 // global variables
   var passwordLength;
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "{", " ] ", "}", "|", "<", ">", "[", "?"];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var upperCaseChoice;
   var lowerCaseChoice;
   var specialChoice;
   var numberChoice;
   var choices;
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "{", " ] ", "}", "|", "<", ">", "[", "?"];
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+ 
 
 
 // Function
